e677855906e8d7e97cd4c2570f745130ad029e79
harryr89 <harry.robertson.harry@gmail.com>
2018-05-14 13:27:27 +0100

    made some changes

--- js/app.js
+++ js/app.js
@@ -1,15 +1,17 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
-    navigation: true,
-    navigationTooltips: ['Home', 'About Me', 'Work', 'Contact'],
-    showActiveTooltip: true,
-    slidesNavigation: true,
-    responsiveWidth: 1024,
-    responsiveHeight: 700
+    // navigation: true,
+    // navigationTooltips: ['Home', 'About Me', 'Work', 'Contact', 'this'],
+    // showActiveTooltip: true,
+    // slidesNavigation: true,
+    // responsiveWidth: 1024,
+    // responsiveHeight: 100
+    autoScrolling: false,
+	  fitToSection: false
   });
 
   var typed = new Typed('.type', {
-    strings: ['I am a web developer.', 'I am a gamer.', 'I am a home barista.', 'I am a hiker.', 'I am a pharmacist.', 'I am a lifelong learner.'],
+    strings: ['I\'m a web developer.', 'I\'m a musician.', 'I\'m a sportsman.', 'I\'m a lifelong learner.'],
     typeSpeed: 40,
     smartBackspace: true,
     loop: true
